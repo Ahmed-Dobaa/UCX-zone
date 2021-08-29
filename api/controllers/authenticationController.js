525b4b7927fe9ff0a32aedd60da38e75a7caaf2c
ahmedmobasher <ahmedmobasher00@gmail.com>
2021-08-29 20:48:17 +0200

    comment email sending api

--- api/controllers/authenticationController.js
+++ api/controllers/authenticationController.js
@@ -33,10 +33,8 @@ module.exports = {
       const privateAttributes = ['password', 'activationToken', 'secret', 'country'];
 
       createdUser = _.omit(createdUser.dataValues, privateAttributes);
-
-      await Mailer.sendUserActivationMail(request.payload.email, activationToken);
-
-      return reply.response(createdUser).code(201);
+      // await Mailer.sendUserActivationMail(request.payload.email, activationToken);
+     return reply.response(createdUser).code(201);
     }
     catch (e) {
       console.log('error', e);
