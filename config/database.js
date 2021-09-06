843745fb1dfe7e154b942210be75427d8cd201cf
Ahmed-Dobaa <engahmeddobaa@gmail.com>
2021-09-06 13:34:52 +0200

    db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  database: 'sql5431252',
-  username: 'sql5431252',
-  password: 'JmeY3URIRj',
-  host: 'sql5.freemysqlhosting.net',
+  database: 'ucxzone_ucxz',//'sql5431252',
+  username: 'ucxzone_remote', //'sql5431252',
+  password: 'wW4Fds9v', //'JmeY3URIRj',
+  host: 'localhost', //'sql5.freemysqlhosting.net',
   port: 3306,
   dialect: 'mysql',
   debug: true,
